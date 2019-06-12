a3617da8aefe0a2d0231688b6f0bb71e7e356725
Elyakim Klettke Brito <elyakimklettke@gmail.com>
2019-06-12 00:02:23 -0300

    Gerador de números aleatórios em lista.

---
+++ gerador.py
@@ -0,0 +1,18 @@
+import random, math
+
+def gerador():
+ numero_aleatorio1 =  (math.ceil(10*random.random()))
+ numero_aleatorio2 =  (math.ceil(10*random.random()))
+ numero_aleatorio3 =  (math.ceil(10*random.random()))
+ numero_aleatorio4 =  (math.ceil(10*random.random()))
+ numero_aleatorio5 =  (math.ceil(10*random.random()))
+
+ lista_random_number = ((numero_aleatorio1), (numero_aleatorio2),(numero_aleatorio3),(numero_aleatorio4),(numero_aleatorio5))
+ print (lista_random_number)
+gerador()
+
+
+while (gerador()) != (00000):
+    gerador()
+#oi
+    
