6c83c1ca037d168394531f261a2fa3988d992195
Elyakim Klettke Brito <elyakimklettke@gmail.com>
2019-06-15 12:10:22 -0300

    Módulo de um telefone, que pode ser importado e alterado no programa principal.

--- gerador.py
+++ gerador.py
@@ -1,4 +1,4 @@
-'''gerador de numeros aleatórios'''
+'''Gerador de números de telefone aleatórios'''
 
 '''
 prefixos:
@@ -10,21 +10,13 @@ OI 84,85,86,87,88,89 '''
 
 
 
+class NumeroDeTelefone:
 
+ def __init__(self):
+  self.ddd = (0)
+  self.nove = (9)
+  self.telefone = (0)
+  '''for (x) in range(86000000,87000000):
+   print (ddddf, nove, x)'''
 
-import random, math
-
-def NumeroOi():
- ddddf = (61)
- nove = (9)
- telefone = (0000000)
- prefixo1 = (9)
-
- for (x) in range(86000000,87000000):
-  print (ddddf, nove, x)
-
-
-
-
-NumeroOi()
 
