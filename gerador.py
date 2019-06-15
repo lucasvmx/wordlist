536d41570749ff2fdea1a2a53b0c92eda1d97413
Elyakim Klettke Brito <elyakimklettke@gmail.com>
2019-06-15 11:48:21 -0300

    gerador de numeros das operador, a ser modificado

--- gerador.py
+++ gerador.py
@@ -1,22 +1,30 @@
 '''gerador de numeros aleatórios'''
 
+'''
+prefixos:
+
+VIVO 67,71,72,95,96,97,98,99
+CLARO 68,73,74,75,76,91,92,93,94
+TIM 69,79,80,81,82,83
+OI 84,85,86,87,88,89 '''
+
 
 
 
 
 import random, math
 
-def gerador():
- numero_aleatorio1 =  (math.ceil(10*random.random()))
- numero_aleatorio2 =  (math.ceil(10*random.random()))
- numero_aleatorio3 =  (math.ceil(10*random.random()))
- numero_aleatorio4 =  (math.ceil(10*random.random()))
- numero_aleatorio5 =  (math.ceil(10*random.random()))
+def NumeroOi():
+ ddddf = (61)
+ nove = (9)
+ telefone = (0000000)
+ prefixo1 = (9)
 
- lista_random_number = ((numero_aleatorio1), (numero_aleatorio2),(numero_aleatorio3),(numero_aleatorio4),(numero_aleatorio5))
- print (lista_random_number)
-gerador()
+ for (x) in range(86000000,87000000):
+  print (ddddf, nove, x)
 
 
-while (gerador()) != (00000):
-    gerador()
+
+
+NumeroOi()
+
