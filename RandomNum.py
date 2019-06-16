1d569e589bf8590a8e38abb6c82db7b91db31402
Elyakim Klettke Brito <elyakimklettke@gmail.com>
2019-06-16 02:39:48 -0300

    Módulo de números telefônicos aleatório.

---
+++ RandomNum.py
@@ -0,0 +1,39 @@
+'''Gerador de números de telefone aleatórios'''
+
+'''
+prefixos:
+
+VIVO 67,71,72,95,96,97,98,99
+CLARO 68,73,74,75,76,91,92,93,94
+TIM 69,79,80,81,82,83
+OI 84,85,86,87,88,89 '''
+
+
+
+class NumeroDeTelefone:
+
+
+    def gerador(self):
+
+        arquivo = open("list", 'w')
+
+        nove = 9
+
+        prefixo = int(input("Entre com o prefixo de sua operadora:\n"))
+
+        for (x) in range(0,10000000):
+            print ("%d%d%06d"%(nove,prefixo,x))
+
+            arquivo.write("%d%d%06d\n"%(nove,prefixo,x))
+        arquivo.close()
+
+
+
+
+
+numero = NumeroDeTelefone()
+
+numero.gerador()
+
+
+
