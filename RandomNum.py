7ece8ec97a4a3d272c266e5810ca05eb31932fa0
Elyakim Klettke Brito <elyakimklettke@gmail.com>
2019-06-16 04:07:42 -0300

    p

--- RandomNum.py
+++ RandomNum.py
@@ -9,21 +9,23 @@ TIM 69,79,80,81,82,83
 OI 84,85,86,87,88,89 '''
 
 
+class numero:
+    def __init__(self):
+        self.prefixo = (0)
+
+    def validar_prefixo(self, prefixo):
+        if (prefixo > 99 or prefixo < 10):
+            return False
+        return True
 
 class NumeroDeTelefone:
 
-
     def gerador(self):
-
         arquivo = open("list", 'w')
-
         nove = 9
-
-        prefixo = int(input("Entre com o prefixo de sua operadora:\n"))
 
         for (x) in range(0,10000000):
             print ("%d%d%06d"%(nove,prefixo,x))
-
             arquivo.write("%d%d%06d\n"%(nove,prefixo,x))
         arquivo.close()
 
